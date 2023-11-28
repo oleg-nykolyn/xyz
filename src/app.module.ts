f4f4bd21697c3a24ad45d2c7547688b6edd06fd3
Oleg Nykolyn <oleg-nykolyn@hotmail.com>
2023-11-28 19:40:51 +0100

    wip

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
-import { MetadataAclModule } from './modules/metadata-acl/metadata-acl.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { MetadataModule } from './modules/metadata/metadata.module';
 
 @Module({
   controllers: [],
   providers: [],
-  imports: [MetadataAclModule, AuthModule, MetadataModule],
+  imports: [AuthModule, MetadataModule],
 })
 export class AppModule {}
