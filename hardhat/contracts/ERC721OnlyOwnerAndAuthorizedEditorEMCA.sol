99a13b88e0cfd8fc29d3d6d35b2bc418e5251406
Oleg Nykolyn <oleg-nykolyn@hotmail.com>
2023-11-29 09:01:17 +0100

    wip: customize erc721 impl

--- hardhat/contracts/ERC721OnlyOwnerAndAuthorizedEditorEMCA.sol
+++ hardhat/contracts/ERC721OnlyOwnerAndAuthorizedEditorEMCA.sol
@@ -2,15 +2,24 @@
 pragma solidity ^0.8.2;
 
 import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
+import '@openzeppelin/contracts/access/Ownable.sol';
 import './EntityMetadataCrudAcl.sol';
 
 contract ERC721OnlyOwnerAndAuthorizedEditorEMCA is
     ERC721,
+    Ownable,
     EntityMetadataCrudAcl
 {
-    constructor() ERC721('erc721-name', 'erc721-symbol') {
-        for (uint256 i = 1; i <= 10; i++) {
+    mapping(address => mapping(uint256 => bool))
+        private _entityMetadataAuthorizedEditors;
+
+    constructor() ERC721('erc721-name', 'erc721-symbol') Ownable(msg.sender) {
+        // Mint 10 tokens for msg.sender.
+        for (uint256 i = 0; i < 10; i++) {
             _mint(msg.sender, i);
+
+            // Set owner as authorized editor for all tokens.
+            _entityMetadataAuthorizedEditors[msg.sender][i] = true;
         }
     }
 
@@ -18,6 +27,7 @@ contract ERC721OnlyOwnerAndAuthorizedEditorEMCA is
         address account,
         uint256 entityId
     ) external view override returns (bool) {
+        // Only owner can read token metadata.
         return ownerOf(entityId) == account;
     }
 
@@ -25,20 +35,31 @@ contract ERC721OnlyOwnerAndAuthorizedEditorEMCA is
         address account,
         uint256 entityId
     ) external view override returns (bool) {
-        return ownerOf(entityId) == account;
+        // Only authorized editor can create token metadata.
+        return _entityMetadataAuthorizedEditors[account][entityId];
     }
 
     function canUpdateEntityMetadata(
         address account,
         uint256 entityId
-    ) external view override returns (bool) {
-        return ownerOf(entityId) == account;
+    ) external pure override returns (bool) {
+        // No one can update token metadata.
+        return false;
     }
 
     function canDeleteEntityMetadata(
         address account,
         uint256 entityId
-    ) external view override returns (bool) {
-        return ownerOf(entityId) == account;
+    ) external pure override returns (bool) {
+        // No one can delete token metadata.
+        return false;
+    }
+
+    function setEntityMetadataAuthorizedEditor(
+        address account,
+        uint256 entityId,
+        bool status
+    ) external onlyOwner {
+        _entityMetadataAuthorizedEditors[account][entityId] = status;
     }
 }
