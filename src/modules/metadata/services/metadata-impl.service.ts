87b02a3c86079161ca4603190c30a1959f504d03
Oleg Nykolyn <oleg-nykolyn@hotmail.com>
2023-11-30 17:39:28 +0100

    wip

--- src/modules/metadata/services/metadata-impl.service.ts
+++ src/modules/metadata/services/metadata-impl.service.ts
@@ -16,80 +16,112 @@ export class MetadataServiceImpl implements MetadataService {
   ) {}
 
   findMetadata(request: FindMetadataRequest): Promise<Metadata[]> {
+    // TODO ...
+    this.logger.log(request);
     throw new Error('Method not implemented.');
   }
 
   async getMetadata(accountAddress: string, id: MetadataId): Promise<Metadata> {
-    const { chain, contractAddress, entityId } = id;
-    const canRead =
-      await this.entityMetadataCrudAclService.canReadEntityMetadata({
-        chain,
-        contractAddress,
-        accountAddress,
-        entityId,
-      });
+    try {
+      const { chain, contractAddress, entityId } = id;
+      const canRead =
+        await this.entityMetadataCrudAclService.canReadEntityMetadata({
+          chain,
+          contractAddress,
+          accountAddress,
+          entityId,
+        });
 
-    if (!canRead) {
-      throw new Error('Unauthorized');
+      if (!canRead) {
+        throw new Error('Unauthorized');
+      }
+
+      const metadata = await this.metadataRepository.get(
+        this.dataSource.manager,
+        id,
+      );
+      return metadata;
+    } catch (error) {
+      // TODO ...
+      this.logger.error(error);
+      throw error;
     }
-
-    return this.metadataRepository.get(this.dataSource.manager, id);
   }
 
   async createMetadata(
     accountAddress: string,
     metadata: Metadata,
   ): Promise<Metadata> {
-    const { chain, contractAddress, entityId } = metadata.getId();
-    const canCreate =
-      await this.entityMetadataCrudAclService.canCreateEntityMetadata({
-        chain,
-        contractAddress,
-        accountAddress,
-        entityId,
-      });
+    try {
+      const { chain, contractAddress, entityId } = metadata.getId();
+      const canCreate =
+        await this.entityMetadataCrudAclService.canCreateEntityMetadata({
+          chain,
+          contractAddress,
+          accountAddress,
+          entityId,
+        });
 
-    if (!canCreate) {
-      throw new Error('Unauthorized');
+      if (!canCreate) {
+        throw new Error('Unauthorized');
+      }
+
+      await this.metadataRepository.save(this.dataSource.manager, metadata);
+      return metadata;
+    } catch (error) {
+      // TODO ...
+      this.logger.error(error);
+      throw error;
     }
-
-    return this.metadataRepository.save(this.dataSource.manager, metadata);
   }
 
   async updateMetadata(
     accountAddress: string,
     metadata: Metadata,
   ): Promise<Metadata> {
-    const { chain, contractAddress, entityId } = metadata.getId();
-    const canUpdate =
-      await this.entityMetadataCrudAclService.canUpdateEntityMetadata({
-        chain,
-        contractAddress,
-        accountAddress,
-        entityId,
-      });
+    try {
+      const { chain, contractAddress, entityId } = metadata.getId();
+      const canUpdate =
+        await this.entityMetadataCrudAclService.canUpdateEntityMetadata({
+          chain,
+          contractAddress,
+          accountAddress,
+          entityId,
+        });
 
-    if (!canUpdate) {
-      throw new Error('Unauthorized');
+      if (!canUpdate) {
+        throw new Error('Unauthorized');
+      }
+
+      await this.metadataRepository.update(this.dataSource.manager, metadata);
+      return metadata;
+    } catch (error) {
+      // TODO ...
+      this.logger.error(error);
+      throw error;
     }
-
-    return this.metadataRepository.update(this.dataSource.manager, metadata);
   }
 
   async deleteMetadata(accountAddress: string, id: MetadataId): Promise<void> {
-    const { chain, contractAddress, entityId } = id;
-    const canDelete =
-      await this.entityMetadataCrudAclService.canDeleteEntityMetadata({
-        chain,
-        contractAddress,
-        accountAddress,
-        entityId,
-      });
+    try {
+      const { chain, contractAddress, entityId } = id;
+      const canDelete =
+        await this.entityMetadataCrudAclService.canDeleteEntityMetadata({
+          chain,
+          contractAddress,
+          accountAddress,
+          entityId,
+        });
 
-    if (!canDelete) {
-      throw new Error('Unauthorized');
+      if (!canDelete) {
+        throw new Error('Unauthorized');
+      }
+
+      await this.metadataRepository.delete(this.dataSource.manager, id);
+    } catch (error) {
+      // TODO ...
+      this.logger.error(error);
+      throw error;
     }
-
-    return this.metadataRepository.delete(this.dataSource.manager, id);
   }
 }
