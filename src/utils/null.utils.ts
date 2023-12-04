1a4da2f3d75ec32c803b5edf22682eca13025657
Oleg Nykolyn <oleg-nykolyn@hotmail.com>
2023-12-04 10:34:50 +0100

    wip

--- src/utils/null.utils.ts
+++ src/utils/null.utils.ts
@@ -1,5 +1,6 @@
 export class NullUtils {
   static mapToUndefinedIfAbsent<T>(value: T): T | undefined {
+    // == is used to check for both null and undefined
     return value == null ? undefined : value;
   }
 }
